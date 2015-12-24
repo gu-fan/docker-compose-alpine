f9fcf52926647eee59653a9943d18a37e25df64a
顾凡 <gu.fan@immomo.com>
2015-12-24 15:34:54 +0800

    - add backup and restore

--- web/app/server/routes/index.js
+++ web/app/server/routes/index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//     console.log('hhhh')
-//     // res.redirect('index')
-//     res.render('index')
-// }); 
+var redis = require('redis');
+
+/* redis */
+var host = process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1';
+var port = process.env.REDIS_PORT_6379_TCP_PORT || 6379;
+var client = redis.createClient(port, host);
+
+router.get('/set', function(req, res, next) {
+  client.set('name', req.query.name, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+
+    res.send('set with: ' + req.query.name)
+
+  });
+}); 
+
+router.get('/get', function(req, res, next) {
+  client.get('name', function(err, result) {
+    if (err) {
+      return next(err);
+    }
+
+    res.send('get with: ' + result)
+  });
+}); 
 
 module.exports = router;
 
